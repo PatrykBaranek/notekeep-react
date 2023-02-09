84bb8326ada72cc59a0a233f6ab6f7d75dcef97c
patrykbaranek <cien2460045@gmail.com>
2023-02-09 16:25:18 +0100

    add notes provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { NotesProvider } from './context/NotesContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <NotesProvider>
+      <App />
+    </NotesProvider>
   </React.StrictMode>
 );
