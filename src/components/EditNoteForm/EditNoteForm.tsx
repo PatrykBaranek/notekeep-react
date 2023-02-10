334486dc1a58b343cb196de9524de8435c998467
patrykbaranek <cien2460045@gmail.com>
2023-02-10 17:18:54 +0100

    add edit form

---
+++ src/components/EditNoteForm/EditNoteForm.tsx
@@ -0,0 +1,77 @@
+import { Button, Form } from 'react-bootstrap';
+import Note, { INote } from '../../model/Note';
+import { useContext, useRef } from 'react';
+import NoteList from '../../model/NoteList';
+import { NotesContext } from '../../context/NotesContext';
+
+interface EditNoteFormProps {
+  note: INote;
+  handleCloseEditForm: () => void;
+}
+
+export const EditNoteForm: React.FC<EditNoteFormProps> = ({ note, handleCloseEditForm }) => {
+  const { handleUpdateNote } = useContext(NotesContext);
+
+  const textRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+  const colorRef = useRef<HTMLInputElement>(null);
+  const pinRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const title = textRef.current?.value as string;
+    const description = descriptionRef.current?.value as string;
+    const color = colorRef.current?.value as string;
+    const pin = pinRef.current?.checked as boolean;
+
+    const updatedNote: INote = {
+      id: note.id,
+      title: title,
+      description: description,
+      date: note.date,
+      color: color,
+      isPin: pin,
+      isDone: note.isDone,
+      isList: note.isList,
+    };
+
+    handleUpdateNote(updatedNote);
+    handleCloseEditForm();
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="title">
+        <Form.Label className="text-dark">Title</Form.Label>
+        <Form.Control ref={textRef} type="text" defaultValue={note.title} required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="description">
+        <Form.Label className="text-dark">Description</Form.Label>
+        <Form.Control
+          ref={descriptionRef}
+          as="textarea"
+          rows={3}
+          defaultValue={note.description}
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="color">
+        <Form.Label className="text-dark">Color</Form.Label>
+        <Form.Control ref={colorRef} type="color" defaultValue={note.color} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="pin">
+        <Form.Check
+          ref={pinRef}
+          className="text-dark"
+          type="checkbox"
+          label="Pin?"
+          defaultChecked={note.isPin}
+        />
+      </Form.Group>
+      <Button variant="success" type="submit">
+        Save Changes
+      </Button>
+    </Form>
+  );
+};
--- src/components/NoteDetails/NoteDetails.tsx
+++ src/components/NoteDetails/NoteDetails.tsx
@@ -1,7 +1,9 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { NotesContext } from '../../context/NotesContext';
 import { INote } from '../../model/Note';
+import { ModalComponent } from '../ModalComponent/ModalComponent';
+import { EditNoteForm } from '../EditNoteForm/EditNoteForm';
 
 interface NoteDetailsProps {
   noteId: number;
@@ -10,36 +12,55 @@ interface NoteDetailsProps {
 
 export const NoteDetails: React.FC<NoteDetailsProps> = ({ noteId, handleCloseDetails }) => {
   const { state, handleDeleteNote } = useContext(NotesContext);
+  const [openEditForm, setOpenEditForm] = useState(false);
+
+  const handleOpenEditForm = () => {
+    setOpenEditForm(true);
+  };
+
+  const handleCloseEditForm = () => {
+    setOpenEditForm(false);
+  };
 
   const noteDetails = state.notes.find((note) => note.id === noteId);
 
+  if (openEditForm) {
+    return (
+      <ModalComponent show={openEditForm} handleCloseModal={handleCloseEditForm}>
+        <EditNoteForm note={noteDetails as INote} handleCloseEditForm={handleCloseDetails} />
+      </ModalComponent>
+    );
+  }
+
   return (
-    <Container className="text-dark">
-      <Row>
-        <h2 className="text-center">{noteDetails?.title}</h2>
-      </Row>
-      <Row>
-        <p>{noteDetails?.description}</p>
-      </Row>
-      <Row>
-        <Col>
-          <Button variant="warning" style={{ width: '100%' }}>
-            Edit Note
-          </Button>
-        </Col>
-        <Col>
-          <Button
-            variant="danger"
-            style={{ width: '100%' }}
-            onClick={() => {
-              handleDeleteNote(noteDetails as INote);
-              handleCloseDetails();
-            }}
-          >
-            Delete Note
-          </Button>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container className="text-dark">
+        <Row>
+          <h2 className="text-center">{noteDetails?.title}</h2>
+        </Row>
+        <Row>
+          <p>{noteDetails?.description}</p>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="warning" style={{ width: '100%' }} onClick={handleOpenEditForm}>
+              Edit Note
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="danger"
+              style={{ width: '100%' }}
+              onClick={() => {
+                handleDeleteNote(noteDetails as INote);
+                handleCloseDetails();
+              }}
+            >
+              Delete Note
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
